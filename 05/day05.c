2a19a55ba918872e843a836df0af085d3c71e2b2
FishEggsThe <matthewfleal@gmail.com>
2024-12-11 13:36:10 -0500

    day 5 two stars

--- 05/day05.c
+++ 05/day05.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #define SIZE 2000
 #define LINESIZE 1024
+#define UPDATESIZE 100
 
 void TrimLine(char _line[LINESIZE]) {
     int length = strlen(_line);
@@ -11,14 +12,47 @@ void TrimLine(char _line[LINESIZE]) {
         _line[length-1] = 0;
 }
 
-void ScrutinizeUpdate(const int _rules[SIZE][2], int _rulesSize, int *_total) {
-    for (int i = 0; i < _rulesSize; i++) {}
+int ScrutinizeUpdate(int _rules[SIZE][2], int _rulesSize, int _update[UPDATESIZE], int _updateSize, int *_total) {
+    // Print update because I said so
+    for (int i = 0; i < _updateSize; i++)
+        printf("%d ", _update[i]);
+
+    // Go through each number in the update
+    for (int i = 0; i < _updateSize; i++) {
+        int startNumber = _update[i]; // The left rule number
+
+        // Go through all the rules that apply
+        for (int j = 0; j < _rulesSize; j++) {
+            if (_rules[j][0] != startNumber) { continue; }
+            // else if (_rules[j][0] == startNumber)
+            int endNumber = _rules[j][1]; // The right rule number
+
+            // Go through update, if the rule is broken then return
+            for (int k = 0; k < _updateSize; k++) {
+                if (_update[k] == endNumber) {
+                    // i: left number - k: right number
+                    if (i > k) { 
+                        printf("*** Rule Broken! (%d|%d)\n", _rules[j][0], _rules[j][1]);
+                        int temp = _update[i];
+                        _update[i] = _update[k];
+                        _update[k] = temp;
+                        return 0; 
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("--- Good To Go (%d)\n", _update[_updateSize/2]);
+    *_total += _update[_updateSize/2];
+    return 1;
 }
 
 int main() {
     // Variables
     FILE *file;
-    int total = 0, rulesSize = 0, rules[SIZE][2];
+    int total = 0, fixedTotal = 0, rulesSize = 0, rules[SIZE][2];
     char line[LINESIZE], divider = '|';
 
     // Opening input file
@@ -27,12 +61,13 @@ int main() {
         int index = 0;
         // Getting the next line
         while (fgets(line, LINESIZE, file)) {
+            char* token;
             if (line[0] == '\n') {
                 printf("Switch! (| -> ,)\n");
                 divider = ',';
             } else if (divider == '|') { // If the line is a rule (first half)
                 TrimLine(line);
-                char* token = strtok(line, " | ");
+                token = strtok(line, " | ");
                 rules[rulesSize][0] = atoi(token);
                 token = strtok(NULL, " | ");
                 rules[rulesSize][1] = atoi(token);
@@ -40,7 +75,19 @@ int main() {
                 printf("%d|%d\n", rules[rulesSize][0], rules[rulesSize][1]);
                 rulesSize++;
             } else if (divider == ',') { // If the line is an update (second half)
-                ScrutinizeUpdate(rules, rulesSize, &total);
+                token = strtok(line, " , ");
+                int update[UPDATESIZE], updateSize = 0;
+                while(token != NULL) {
+                    update[updateSize] = atoi(token);
+                    token = strtok(NULL, " , ");
+                    updateSize++;
+                }
+                int pass = ScrutinizeUpdate(rules, rulesSize, update, updateSize, &total);
+                if (pass == 0) {
+                    do {
+                        pass = ScrutinizeUpdate(rules, rulesSize, update, updateSize, &fixedTotal);
+                    } while (pass == 0);
+                }
             }
             
         }
@@ -49,5 +96,6 @@ int main() {
     printf("\n");
     
     printf("Sum of middle numbers: %d\n", total);
+    printf("Sum of fixed numbers: %d\n", fixedTotal);
     return 0;
 }
