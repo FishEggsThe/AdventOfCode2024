32da27a1eafb31f1b0c30d0ceaf0edda89a232e6
FishEggsThe <matthewfleal@gmail.com>
2024-12-13 19:17:40 -0500

    day 7 first star

--- 07/day07.c
+++ 07/day07.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <limits.h>
 #define LINESIZE 1024
 
 void TrimLine(char _line[LINESIZE]) {
@@ -10,25 +11,38 @@ void TrimLine(char _line[LINESIZE]) {
         _line[length-1] = 0;
 }
 
-// int CheckNumber() {
-//     if (CheckNumber() == 1) { return 1; }
-//     if (CheckNumber() == 1) { return 1; }
-// }
+// int CheckNumber(int _checkTotal, int _nums[16], int _numsSize, int _i, int _expTotal) {
+int CheckNumber(int _nums[16], int _numsSize, int _i, int _expTotal, int _checkTotal) {
+    // Checking if at end
+    if (_i >= _numsSize) {
+        if (_checkTotal == _expTotal)
+            return 1; 
+        else
+            return 0;
+    }
+
+    // Check addition then multipliction (Recursive), if either false then return false
+    if (CheckNumber(_nums, _numsSize, _i+1, _expTotal, _checkTotal+_nums[_i]) == 1) { printf("add "); return 1; }
+    if (CheckNumber(_nums, _numsSize, _i+1, _expTotal, _checkTotal*_nums[_i]) == 1) { printf("mul "); return 1; }
+    return 0;
+}
 
 int main() {
     // Variables
     FILE *file;
     char line[LINESIZE];
-    int total = 0, currTotal = 0, numLength = 0, numbers[16];
+    uint64_t total = 0, expectedTotal = 0;
+    int numLength = 0, numbers[16];
 
     // Opening input file
     file = fopen("input.txt", "r");
     if (file) {
         // Getting the next line
         while (fgets(line, LINESIZE, file)) {
+            // Get line data
             TrimLine(line);
             char* token = strtok(line, ":");
-            currTotal = atoi(token); numLength = 0;
+            expectedTotal = atoll(token); numLength = 0;
             
             token = strtok(NULL, " ");
             while(token != NULL) {
@@ -37,17 +51,22 @@ int main() {
                 token = strtok(NULL, " ");
             }
 
-            printf("%d:", currTotal);
+            printf("%llu:\t", expectedTotal);
             for(int i = 0; i < numLength; i++) {
                 printf(" %d", numbers[i]);
             }
             printf("\n");
             
+            // Do math
+            int check = CheckNumber(numbers, numLength, 1, expectedTotal, numbers[0]);
+            if (check == 1) { printf("possible :)"); total += expectedTotal; }
+            else { printf("IMPOSSIBLE >:C"); }
+            printf("\n");
         }
         fclose(file);
     }
     printf("\n");
     
-    printf("total: %d\n", total);
+    printf("total: %llu\n", total);
     return 0;
 }
