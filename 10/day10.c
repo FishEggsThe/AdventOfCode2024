29b7ef38fd3ba165cc2ddc0c3e3e9e9b60a2e989
FishEggsThe <matthewfleal@gmail.com>
2024-12-16 22:45:01 -0500

    working on day 10

--- 10/day10.c
+++ 10/day10.c
@@ -4,8 +4,20 @@
 #include <assert.h>
 #define LINESIZE 64
 
+struct xyPos {
+  int x, y;
+};
+
+struct xyPos CreatePos(int _x, int _y) {
+    struct xyPos pos;
+    pos.x = _x;
+    pos.y = _y;
+    return pos;
+}
+
 char grid[LINESIZE][LINESIZE];
-int gridSize = 0;
+int gridSize = 0, total = 0, startPointsSize = 0;
+struct xyPos startPoints[LINESIZE];
 
 void TrimLine(char _line[LINESIZE]) {
     int length = strlen(_line);
@@ -20,11 +32,26 @@ void PrintGrid() {
     printf("\n");
 }
 
+int FindAllValidPaths(struct xyPos _pos, int *_total) {
+    int currNum = grid[_pos.y][_pos.x] - 48;
+    if (currNum >= 9) { *_total += 1; return 1; }
+
+    int dir[4][2] = {{1, 0},
+                     {0, 1},
+                     {-1, 0},
+                     {0, -1},};
+    for(int m = 0; m < 4; m++) {
+        struct xyPos nextPos = CreatePos(_pos.x+dir[m][0], _pos.y+dir[m][1]);
+        int nextNum = grid[nextPos.y][nextPos.x] - 48;
+        if(nextNum == currNum+1) { return FindAllValidPaths(nextPos, _total); }
+    }
+    return 0;
+}
+
 int main() {
     // Variables
     FILE *file;
     char line[LINESIZE];
-    int total = 0;
 
     // Opening input file
     file = fopen("input.txt", "r");
@@ -38,6 +65,22 @@ int main() {
         fclose(file);
     }
     PrintGrid();
+
+    // Get starting points with struct :D!!!
+    for(int i = 0; i < gridSize; i++) {
+        for(int j = 0; j < gridSize; j++) {
+            if (grid[i][j] == '0') {
+                startPoints[startPointsSize] = CreatePos(j, i);
+                printf("%d, %d\n", startPoints[startPointsSize].x, startPoints[startPointsSize].y);
+                startPointsSize++;
+            }
+        }
+    }
+
+    // Find all valid paths
+    for(int i = 0; i < startPointsSize; i++) {
+        FindAllValidPaths(startPoints[i], &total);
+    }
     
     printf("total: %d\n", total);
     return 0;
